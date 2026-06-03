fdae8638201607b155144719b946235fc049a782
iftype <conditionaltype@gmail.com>
2026-06-03 13:42:03 +0900

    refactor: Product  유효성 검증 분리

--- client/src/domain/Product.ts
+++ client/src/domain/Product.ts
@@ -4,6 +4,14 @@ export interface Product {
   price: number;
 }
 
+export const validateName = (name: string) => {
+  return name.length === 0 || name.length > 100;
+};
+
+export const validatePrice = (price: number) => {
+  return price <= 0;
+};
+
 export const postProduct = async ({
   name,
   price,
