79b6b5cb55f127db2fc81d52a4de1aeda940fb35
iftype <conditionaltype@gmail.com>
2026-06-03 12:57:30 +0900

    chore: 파일 위치변경

--- client/src/test/ProductSection.test.tsx
+++ client/src/test/ProductSection.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { beforeEach, describe, expect, it, vi } from "vitest";
-import ProductSection from "./ProductSection";
+import ProductSection from "../components/ProductSection";
 
 beforeEach(() => {
   globalThis.fetch = vi
