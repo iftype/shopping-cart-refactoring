479d2819fe0a139100101168700f0a472e374c56
iftype <conditionaltype@gmail.com>
2026-06-03 13:14:09 +0900

    refactor: Product 인터페이스와 Api 호출을 분리

---
+++ client/src/domain/Product.ts
@@ -0,0 +1,21 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
+
+export const postProduct = async ({
+  name,
+  price,
+}: Pick<Product, "name" | "price">) => {
+  const res = await fetch("/products", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name, price }),
+  });
+  return res.json();
+};
+
+export const deleteProduct = async (id: Pick<Product, "id">) => {
+  await fetch(`/products/${id}`, { method: "DELETE" });
+};
